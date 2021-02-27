438fff4201755037e2c5c9ee5684e1a6983ed31b
Pavel Vasev <pavel.vasev@gmail.com>
2021-02-28 03:56:48 +0500

    spheres and lines interp

--- views/vz-lines.js
+++ views/vz-lines.js
@@ -1,4 +1,5 @@
 import parse_csv from "../src/csv.js";
+import * as utils from "../src/utils.js";
 
 export function setup( vz ) {
   vz.addItemType( "cinema-view-lines","Cinema: lines", function( opts ) {
@@ -11,35 +12,15 @@ export function create( vz, opts ) {
   var obj = vz.createObj( opts );
   var gr  = vz.vis.addLines( obj, "lines" );
   
-  obj.addFile( "file","",function(v) {
-    loadFile( v, function(res) {
-      var dat = parse_csv( res );
-      
-      gr.positions = combine( [ dat.X, dat.Y, dat.Z, dat.X2, dat.Y2, dat.Z2 ] );
-      gr.colors = combine( [ dat.R, dat.G, dat.B,dat.R2, dat.G2, dat.B2 ] );
-      gr.radiuses = dat.RADIUS || [];
-      
-    },
-    function(err) {
-      gr.positions = [];
-    });
+  obj.trackParam( "@dat",function(v) {
+    var dat = obj.getParam("@dat");
+    gr.positions = utils.combine( [ dat.X, dat.Y, dat.Z, dat.X2, dat.Y2, dat.Z2 ] );
+    gr.colors = utils.combine( [ dat.R, dat.G, dat.B,dat.R2, dat.G2, dat.B2 ] );
+    gr.radiuses = dat.RADIUS || [];
+    obj.signal("changed");
   });
-  
-  // todo: add interpolation, like in points
+
+  utils.file_merge_feature( obj,parse_csv, utils.interp_csv, "@dat" );
 
   return obj;
-}
-
-function combine( arrays_list ) {
-  if (!Array.isArray(arrays_list[0])) return [];
-  
-  var res = [];
-  var len = arrays_list[0].length;
-  for (var i=0; i<len; i++) {
-    for (var j=0; j<arrays_list.length; j++) {
-      var arr = arrays_list[j];
-      res.push( arr ? arr[i] : 0 );
-    }
-  }
-  return res;
 }
