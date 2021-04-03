a3b0245f166b69bdde818f93005d609522f81fe2
Pavel Vasev <pavel.vasev@gmail.com>
2021-04-03 17:43:17 +0500

    browser counterpart for local data server

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// на самом деле этот код вот в этом файле это часть index.html по своему существу, т.е. часть точки входа в приложение
+// и по уму там и надо писать.. а то раскидан слой по 2м файлам
+
+//
 import * as cv from "./src/cinema-viewzavr.js";
 
 export function setup( vz ) {
@@ -6,6 +10,7 @@ export function setup( vz ) {
 
 export function create( vz, opts ) {
   var ci = cv.create( vz, opts );
-  ci.setParam("file","./examples/_tutorial/0-points-fly.cdb/data.csv");
+//  var file = getParameterByName("datapath") || "./examples/_tutorial/0-points-fly.cdb/data.csv";
+//  ci.setParam("file",file);
   return ci;
 }
