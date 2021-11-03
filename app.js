c6737655f822803b53061d92003e8e742dfd085f
Pavel Vasev <pavel.vasev@gmail.com>
2021-11-03 17:13:38 +0500

    design

--- app.js
+++ app.js
@@ -7,10 +7,35 @@ export function create( vz, opts ) {
   opts.name ||= "vr-cinema";
   var obj = vz.createObjByType( "cinema-view-cinema",opts );
 
+  vzPlayer.feature("css-styles");
+  vzPlayer.addStyle(`
+.fine-bg-color {
+  background: rgb(99 116 137 / 36%);
+}
+.fine-padding {
+  padding: 5px;
+}
+.shift-padding {
+  padding: 0.2em 0.2em 0.2em 0.4em;
+}
+.vertical-auto-scroll {
+  overflow-y: auto; max-height:95vh;
+}
+.screen-two-side-columns {
+  justify-content: space-between;
+  width:100%;
+  pointer-events:none;
+}
+.screen-two-side-columns > div {
+  pointer-events:all;
+}
+    `
+    )
+
   vz.feature("xml-lang");
   var screen1 = vz.createFromXmlNow(`  
-<screen id="screen1" justify-content="space-between" style='width:100%; pointer-events:none'>
-  <column id="left_column" gap="1em" margin="2em" style='pointer-events:all;'>
+<screen id="screen1" class="screen-two-side-columns">
+  <column id="left_column" gap="1em" margin="2em" class="vertical-auto-scroll">
     <text id="text" text="Cinema database" >
     </text>
     <edit-params id="edit-params" input="/" only2="file interpolation refresh" >
@@ -21,23 +46,28 @@ export function create( vz, opts ) {
     </edit-params>
 
     <btn id="btn_tg" text="Visual objects" cmd="../oguis->trigger_visible" style='width:170px'/>
-    <column id="oguis" gap='0.2em'>
+    <column id="oguis" gap='0.2em' class="shift-padding fine-bg-color">
       <objects-guis objects="**/FILE*/* @cinema-visual"/>
     </column>
 
-   <objects-guis objects="/Animation_player"/>
+    <text id="text_3" text="Animations" >
+    </text>
+    <column id="oguis" gap='0.2em' class="shift-padding fine-bg-color">
+      <objects-guis objects="@animation-player" />
+    </column>
 
   </column>
   
 
-  <column id="right_column" gap="1em" margin="2em" style='pointer-events:all;'>
+  <column id="right_column" gap="1em" margin="2em" class="vertical-auto-scroll">
 
-    <btn text="Add extra" cmd="../adder->showModal"/>
-    <gui-dialog-add id="adder" add-features="cinema-extra"/>
+    <column class="fine-padding">
     <btn id="btn_tg" text="Extras" cmd="../eguis->trigger_visible" style='width:170px'/>
-
-    <column id="eguis" gap='0.2em'>
-    <objects-guis objects="@cinema-extra" removable="true"/>
+    <column id="eguis" gap='0.2em' class="shift-padding fine-bg-color">
+      <btn text="+ add new" cmd="../adder->showModal"/>
+      <gui-dialog-add id="adder" add-features="cinema-extra"/>
+      <objects-guis objects="@cinema-extra" removable="true"/>
+    </column>
     </column>
 
   </column>
@@ -46,10 +76,9 @@ export function create( vz, opts ) {
 `,null,obj,"screen1");
 
   screen1.activate();
+ 
 
-  vz.register_feature( "cinema-extra", () => {} );
-
-  var ap = vz.createObj({feature:"animation-player",name:"Animation_player",parent:obj})
+  var ap = vz.createObj({feature:"animation-player cinema-extra",name:"Animation_player",parent:obj})
   //ap.feature("animation-player");
 
 //  findObjects( ":artefact", ... ) repeater.input = [....];
